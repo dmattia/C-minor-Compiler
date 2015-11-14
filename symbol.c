a6b7211f3154304c7f89001abde543480c2b71bb
dmattia <dmattia@nd.edu>
2015-11-14 13:08:52 -0500

    Added hash table library \n\n made slight gitignore updates

--- symbol.c
+++ symbol.c
@@ -1,1 +1,11 @@
 #include "symbol.h"
+#include "stdlib.h"
+
+struct symbol * symbol_create( symbol_t kind, struct type *type, char *name ) {
+	struct symbol *s;
+	s = (struct symbol*)malloc(sizeof(struct symbol));
+	s->kind = kind;
+	s->type = type;
+	s->name = name;
+	return s;
+}
